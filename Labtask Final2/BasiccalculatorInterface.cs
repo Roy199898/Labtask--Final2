0509caea2379dc9c355dae730d16430d82be2854
Roy199898 <dipr19989@gmail.com>
2020-11-25 13:26:19 +0600

    Test Interface

---
+++ Labtask Final2/BasiccalculatorInterface.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labtask_Final2
+{
+    interface BasiccalculatorInterface
+    {
+        int sum(int x, int y);
+        int sub(int x, int y);
+        int multiplication(int x, int y);
+        int division(int x, int y);
+    }
+    class BasicCalc : BasiccalculatorInterface
+    {
+        int result;
+        public int sum(int number1, int number2)
+        {
+            result = number1 + number2;
+            return result;
+        }
+        public int sub(int number1, int number2)
+        {
+            result = number1 - number2;
+            return result;
+        }
+        public int multiplication(int number1, int number2)
+        {
+            result = number1 * number2;
+            return result;
+        }
+        public int division(int number1, int number2)
+        {
+            result = number1 / number2;
+            return result;
+        }
+        public void printStates()
+        {
+            Console.WriteLine("Result : " + result);
+        }
+    }
+}
